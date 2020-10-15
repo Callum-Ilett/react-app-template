81b10b16b352e06a3cec7e56ac727dc3c71f15ec
Ilettc <callum_i@yahoo.com>
2020-10-15 10:56:19 +0100

    cd into public

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ if [ $TRAVIS_BRANCH == 'main' ] ; then
   eval "$(ssh-agent -s)"
   ssh-add
   npm run build
-  rsync -rq --delete --rsync-path="mkdir -p react-app && rsync" \
+  rsync -rq --delete --rsync-path="cd public && rsync" \
   $TRAVIS_BUILD_DIR travis@142.93.36.116:react-app
 else
   echo "Not deploying, since this branch isn't main."
