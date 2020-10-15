a70718c3fad2de3dce8ecf1b9fadf8d3b32ac5af
Ilettc <callum_i@yahoo.com>
2020-10-15 11:22:56 +0100

    add to callum user

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,9 @@ set -xe
 
 if [ $TRAVIS_BRANCH == 'main' ] ; then
   eval "$(ssh-agent -s)"
-  ssh-add
+  ssh-add ~/.ssh/id_rsa
   npm run build
-  rsync -rq --delete --rsync-path="cd public && rsync" \
-  $TRAVIS_BUILD_DIR travis@142.93.36.116:testing
+  rsync -a --exclude={'/node_modules','/src','/public'} build/ travis@142.93.36.116:/home/callum/hello
 else
-  echo "Not deploying, since this branch isn't main."
+  echo "Not deploying, since the branch isn't main."
 fi
