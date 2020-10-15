8577a245fac9fe8d24eeeb1188ec46a3bb112b62
Ilettc <callum_i@yahoo.com>
2020-10-15 11:32:10 +0100

    fixed user permissions

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,10 @@ set -xe
 
 if [ $TRAVIS_BRANCH == 'main' ] ; then
   eval "$(ssh-agent -s)"
-  ssh-add ~/.ssh/id_rsa
+  ssh-add
   npm run build
-  rsync -a --exclude={'/node_modules','/src','/public'} build/ travis@142.93.36.116:/home/callum/hello
+  rsync -rq --delete --rsync-path="mkdir -p react-app && rsync" \
+  $TRAVIS_BUILD_DIR/public travis@142.93.36.116:/home/callum/react-app
 else
-  echo "Not deploying, since the branch isn't main."
+  echo "Not deploying, since this branch isn't main."
 fi
